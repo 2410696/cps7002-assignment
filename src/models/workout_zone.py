702c287fc652afe4d55b279aa9c41314c5b02626
2410696 <2410696@live.stmarys.ac.uk>
2024-12-24 17:00:06 +0500

    Added the models for all the modules

--- src/models/workout_zone.py
+++ src/models/workout_zone.py
@@ -1,46 +1,31 @@
 # src/models/workout_zone.py
 
 class WorkoutZone:
-    def __init__(self, zone_id, name, category, equipment_list, attendant_name):
-        """
-        Initializes a new WorkoutZone instance.
-        :param zone_id: Unique identifier for the workout zone
-        :param name: Name of the workout zone
-        :param category: Type/category of the workout zone (e.g., cardio, weightlifting)
-        :param equipment_list: List of equipment available in the workout zone
-        :param attendant_name: Name of the zone attendant
-        """
+    def __init__(self, zone_id, zone_type, attendant_name, updates, schedules, promotions):
         self.zone_id = zone_id
-        self.name = name
-        self.category = category
-        self.equipment_list = equipment_list
+        self.zone_type = zone_type
         self.attendant_name = attendant_name
-        self.schedules = []
+        self.updates = updates
+        self.schedules = schedules
+        self.promotions = promotions
 
-    def add_schedule(self, schedule):
-        """
-        Adds a schedule to the workout zone.
-        :param schedule: Dictionary containing schedule details
-        """
-        self.schedules.append(schedule)
-
-    def remove_schedule(self, schedule_id):
-        """
-        Removes a schedule by its ID.
-        :param schedule_id: Unique identifier for the schedule
-        """
-        self.schedules = [schedule for schedule in self.schedules if schedule["id"] != schedule_id]
-
-    def get_info(self):
-        """
-        Retrieves workout zone details as a dictionary.
-        :return: Dictionary with workout zone details
-        """
+    def to_dict(self):
         return {
             "zone_id": self.zone_id,
-            "name": self.name,
-            "category": self.category,
-            "equipment_list": self.equipment_list,
+            "zone_type": self.zone_type,
             "attendant_name": self.attendant_name,
+            "updates": self.updates,
             "schedules": self.schedules,
+            "promotions": self.promotions
         }
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            data["zone_id"],
+            data["zone_type"],
+            data["attendant_name"],
+            data["updates"],
+            data["schedules"],
+            data["promotions"]
+        )
